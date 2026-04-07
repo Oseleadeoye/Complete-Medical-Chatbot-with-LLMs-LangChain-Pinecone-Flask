bac48e5da386d2a0c117115dceadff4f16202b6a
Osele Adeoye <130908260+Oseleadeoye@users.noreply.github.com>
2026-04-07 08:41:01 -0600

    feat: initialize project dependencies and package configuration files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='medicalchatbot',
+    version='0.1.0',
+    author='Osele Adeoye',
+    author_email="oseleadeoye@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
+
+
